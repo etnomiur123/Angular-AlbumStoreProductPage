fab44a17ae3ba5667bca6820581a9fb2bc659a70
Rui Monte <CTW01116@criticaltechworks.com>
2021-01-21 17:24:23 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators/map';
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 @Injectable()
 export class ProductService {
 
@@ -9,7 +10,9 @@ export class ProductService {
   constructor(private _http: Http) {}
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response .json());
+    return this._http.get(this._albumUrl).pipe(
+      map(response => response .json())
+    )
   }
 
 }
