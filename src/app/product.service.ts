dc513aa3d014713d694df5d02b3e4ccd94eaa676
Rui Monte <CTW01116@criticaltechworks.com>
2021-01-21 17:12:49 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators/map';
 @Injectable()
 export class ProductService {
 
